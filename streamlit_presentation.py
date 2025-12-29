b11dc6889e0f6ccd3ad1f5f8e919e4f2e50d0ce9
VvDustvV <vendula.marcikova@outlook.com>
2025-12-29 12:13:09 +0100

    Update streamlit_presentation.py

--- streamlit_presentation.py
+++ streamlit_presentation.py
@@ -540,19 +540,19 @@ elif st.session_state.active_dashboard == 'priciny':
             st.divider()
 
             st.subheader("Pohlaví chodců zapletených do dopravních nehod")
-            category_conseq(df_but3[df_but3['responsible_party']=='Pedestrian'], 'Pedestrian', 'responsible_party', 'pedestrian_category', 'bar')
+            category_conseq(df_but3[df_but3['responsible_party']=='Pedestrian'].reset_index(), 'Pedestrian', 'responsible_party', 'pedestrian_category', 'bar')
             st.divider()
             st.text("Nejvíce s auty na silnicích střetávají muži. U žen je to výrazně nižší číslo.")
 
             st.divider()
 
             st.subheader("Poměr chodců s reflexním vybavením")
-            category_conseq(df_but3[df_but3['responsible_party']=='Pedestrian'], 'Pedestrian', 'responsible_party', 'pedestrian_reflective_elements', 'pie')
+            category_conseq(df_but3[df_but3['responsible_party']=='Pedestrian'].reset_index(), 'Pedestrian', 'responsible_party', 'pedestrian_reflective_elements', 'pie')
             st.text("Z této informace je zřejmé, že je absence reflexních prvků u chodců zapetených do dopravní nehody takřka pravidlem")
 
         elif selected_cause == 'Driver motor vehicle':
             st.subheader('Poměr následků')
-            category_conseq(df_but3, selected_cause, 'responsible_party', 'accident_characteristic', 'pie')
+            category_conseq(df_but3[df_but3['responsible_party']=='Driver motor vehicle'].reset_index(), 'Driver motor vehicle', 'responsible_party', 'accident_characteristic', 'pie')
             st.subheader("Nejřastější chyby řičičů")
             category_conseq(df_but3[df_but3['responsible_party']=='Driver motor vehicle'].reset_index(), 'Driver motor vehicle', 'responsible_party', 'main_cause', 'bar')
 
